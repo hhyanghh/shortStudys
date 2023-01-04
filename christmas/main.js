4c03326dcd3046e16921913e43a22dedf455f157
양현화 <hh.yang@fssuniverse.com>
2023-01-04 22:25:45 +0900

    눈내리기 효과 추가

--- christmas/main.js
+++ christmas/main.js
@@ -6,12 +6,22 @@ function makeSnowflake() {
   const delay = Math.random() * 10;
   const initialOpacity = Math.random();
   const duration = Math.random() * 20 + MIN_DURATION;
+  const blur = Math.random();
 
   snowflake.classList.add("snowflake");
   snowflake.style.left = `${Math.random() * window.screen.width}px`;
   snowflake.style.animationDelay = `${delay}s`;
   snowflake.style.opacity = initialOpacity;
   snowflake.style.animation = `fall ${duration}s linear`;
+  snowflake.style.filter = `blur(${blur}px)`;
+
+  snowflake.addEventListener("mouseover", () => {
+    snowflake.classList.add("mouseover");
+  });
+
+  snowflake.addEventListener("mouseleave", () => {
+    snowflake.classList.remove("mouseover");
+  });
 
   body.appendChild(snowflake);
 
@@ -21,8 +31,6 @@ function makeSnowflake() {
   }, (duration + delay) * 1000);
 }
 
-for (let index = 0; index < 100; index++) {
-  setTimeout(makeSnowflake, 500 * index);
+for (let index = 0; index < 500; index++) {
+  setTimeout(makeSnowflake, 50 * index);
 }
-
-// TODO:눈에 mouse hover시, 애니메이션 추가
